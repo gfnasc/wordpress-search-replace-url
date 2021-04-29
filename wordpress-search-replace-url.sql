5fdbbb62bb0634f94b35337017e0d423e23a0231
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2021-04-29 12:16:42 -0300

    troca de urls

--- wordpress-search-replace-url.sql
+++ wordpress-search-replace-url.sql
@@ -1,4 +1,4 @@
-UPDATE wp_options SET option_value = replace(option_value, 'lizesaude.com.br', 'staging.lizesaude.com.br') WHERE option_name = 'home' OR option_name = 'siteurl';
-UPDATE wp_posts SET guid = replace(guid, 'lizesaude.com.br','staging.lizesaude.com.br');
-UPDATE wp_posts SET post_content = replace(post_content, 'lizesaude.com.br', 'staging.lizesaude.com.br');
-UPDATE wp_postmeta SET meta_value = replace(meta_value,'lizesaude.com.br','staging.lizesaude.com.br');
+UPDATE wp_options SET option_value = replace(option_value, 'oldUrl',  'newUrl') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE wp_posts SET guid = replace(guid, 'oldUrl', 'newUrl');
+UPDATE wp_posts SET post_content = replace(post_content, 'oldUrl',  'newUrl');
+UPDATE wp_postmeta SET meta_value = replace(meta_value,'oldUrl', 'newUrl');
